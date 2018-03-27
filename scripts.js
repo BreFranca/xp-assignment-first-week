5568f9ee33504421976049dcc960db1b74129e88
Breno Franca <bre.sfranca@gmail.com>
2018-03-26 23:47:25 -0300

    Finish contact form

--- scripts.js
+++ scripts.js
@@ -2,3 +2,21 @@
 window.onload = function() {
   document.querySelector('#loader').classList.add("hidden");
 };
+
+document.querySelector('.form-control').addEventListener('focus', hoverInput());
+
+function hoverInput() {
+	document.querySelector('.form-control').classList.add("hidden")
+}
+
+function scrollTo(element) {
+	window.scroll({
+		behavior: 'smooth',
+		left: 0,
+		top: element.offsetTop
+	});
+}
+
+document.getElementById("button").addEventListener('click', () => {
+	scrollTo(document.getElementById("8"));
+});
